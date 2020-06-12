0e0e9ab602088c66619fd742ca5ad5059d5c113b
Yankai Liu <yankai.liu@thoughtworks.com>
2020-06-12 22:37:14 +0800

    create unit test class

---
+++ src/test/java/LockPackage/LockPackageTest.java
@@ -0,0 +1,11 @@
+package LockPackage;
+
+import org.junit.Test;
+
+//TODO Given 还有空柜子， 包  When 存包  Then 存包成功，返回票据
+//TODO Given 没有空柜子，包  When 存包  Then 存包失败，不返回票据
+//TODO Given 合法的票据   When 取包  Then  取包成功
+//TODO Given 不合法的票据 When 取包  Then 取包失败
+public class LockPackageTest {
+    
+}
