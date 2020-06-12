e01a1c34931d8d1f6e5acbc9bcaf3911467f7803
Yankai Liu <yankai.liu@thoughtworks.com>
2020-06-12 23:43:38 +0800

    remove unused code and TODO list

--- src/test/java/Locker/LockerTest.java
+++ src/test/java/Locker/LockerTest.java
@@ -2,14 +2,6 @@ package Locker;
 
 import org.junit.Test;
 import org.junit.Assert;
-import sun.security.krb5.internal.Ticket;
-
-import java.util.concurrent.locks.Lock;
-
-//TODO Given 还有空柜子， 包  When 存包  Then 存包成功，返回票据
-//TODO Given 没有空柜子，包  When 存包  Then 存包失败，不返回票据
-//TODO Given 合法的票据   When 取包  Then  取包成功
-//TODO Given 不合法的票据 When 取包  Then 取包失败
 
 public class LockerTest {
 
